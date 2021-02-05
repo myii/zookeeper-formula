452e2bc75dcd17646002b528b44e0d3a0b636e0f
Imran Iqbal <iqbalmy@hotmail.com>
2021-02-05 11:00:47 +0000

    test: make temporary adjustment for `service` testing only

--- test/integration/default/controls/services_spec.rb
+++ test/integration/default/controls/services_spec.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 # Prepare platform "finger"
-platform_finger = system.platform[:finger].split('.').first.to_s
+# platform_finger = system.platform[:finger].split('.').first.to_s
 
 control 'zookeeper service' do
   impact 0.5
   title 'should be running and enabled'
 
   # Overide by `platform_finger`
-  service_name =
-    case platform_finger
-    when 'centos-6', 'amazonlinux-1'
-      'crond'
-    else
-      'systemd-journald'
-    end
+  service_name = 'zookeeper'
+  # service_name =
+  #   case platform_finger
+  #   when 'centos-6', 'amazonlinux-1'
+  #     'crond'
+  #   else
+  #     'systemd-journald'
+  #   end
 
   describe service(service_name) do
     it { should be_installed }
