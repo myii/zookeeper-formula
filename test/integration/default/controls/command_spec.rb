010ff7412059580489e86d9e068eba0117471cfb
Imran Iqbal <iqbalmy@hotmail.com>
2021-02-06 20:59:12 +0000

    fixup

--- test/integration/default/controls/command_spec.rb
+++ test/integration/default/controls/command_spec.rb
@@ -3,7 +3,7 @@
 control 'zookeeper command to test server running in non-error state' do
   title 'should match desired outputs'
 
-  # https://zookeeper.apache.org/doc/r3.1.2/zookeeperAdmin.html#sc_zkCommands
+  # https://zookeeper.apache.org/doc/r3.6.2/zookeeperAdmin.html#sc_zkCommands
   describe command('echo ruok | nc localhost 2181') do
     its(:stdout) { should eq("imok") }
     its(:stderr) { should be_empty }
